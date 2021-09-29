74112703f29f89e885afb284bd2228fbace5be5f
Pavel Hübner <pavel.hubner@hardwario.com>
2021-09-29 13:19:35 +0200

    Fix missing --softreset option in board.cmake

--- boards/arm/chester_nrf52840/board.cmake
+++ boards/arm/chester_nrf52840/board.cmake
@@ -1,3 +1,4 @@
+board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
