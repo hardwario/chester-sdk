c6535286fb9d4f1c7adfbfdcbcee2f97809f7ee2
Pavel Hübner <pavel.hubner@hardwario.com>
2021-10-10 10:45:12 +0200

    hio_accel: Add implementation

---
+++ include/hio_accel.h
@@ -0,0 +1,8 @@
+#ifndef HIO_ACCEL_H
+#define HIO_ACCEL_H
+
+int hio_accel_init(void);
+int hio_accel_read(float *accel_x, float *accel_y, float *accel_z,
+                   int *orientation);
+
+#endif
