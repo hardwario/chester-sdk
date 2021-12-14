56df1a0f0451222576b067743c30456da9785d1e
Pavel Hübner <pavel.hubner@hardwario.com>
2021-12-14 17:11:45 +0100

    samples: therm: Add sample
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

---
+++ samples/therm/src/main.c
@@ -0,0 +1,44 @@
+/* CHESTER includes */
+#include <ctr_led.h>
+#include <ctr_therm.h>
+
+/* Zephyr includes */
+#include <logging/log.h>
+#include <zephyr.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+void main(void)
+{
+	int ret;
+
+	LOG_INF("Build time: " __DATE__ " " __TIME__);
+
+	ret = ctr_therm_init();
+
+	if (ret < 0) {
+		LOG_ERR("Call `ctr_therm_read` failed: %d", ret);
+		k_oops();
+	}
+
+	for (;;) {
+		LOG_INF("Alive");
+
+		ctr_led_set(CTR_LED_CHANNEL_R, true);
+		k_sleep(K_MSEC(10));
+		ctr_led_set(CTR_LED_CHANNEL_R, false);
+
+		float temperature;
+
+		ret = ctr_therm_read(&temperature);
+
+		if (ret < 0) {
+			LOG_ERR("Call `ctr_therm_read` failed: %d", ret);
+
+		} else {
+			LOG_INF("Temperature: %.1f C", temperature);
+		}
+
+		k_sleep(K_SECONDS(5));
+	}
+}
