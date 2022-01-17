f5779c27ef0737483722a77d68fe07271e52e742
Pavel Hübner <pavel.hubner@hardwario.com>
2022-01-17 21:49:48 +0100

    samples: sleep: Add sample
    Minimalistic sample to allow current measurements.
    
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

---
+++ samples/sleep/src/main.c
@@ -0,0 +1,23 @@
+/* CHESTER includes */
+#include <ctr_led.h>
+
+/* Zephyr includes */
+#include <logging/log.h>
+#include <zephyr.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+void main(void)
+{
+	LOG_INF("Build time: " __DATE__ " " __TIME__);
+
+	for (;;) {
+		LOG_INF("Alive");
+
+		ctr_led_set(CTR_LED_CHANNEL_R, true);
+		k_sleep(K_MSEC(50));
+		ctr_led_set(CTR_LED_CHANNEL_R, false);
+
+		k_sleep(K_SECONDS(10));
+	}
+}
