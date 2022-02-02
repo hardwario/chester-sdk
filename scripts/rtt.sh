e86a67b1314bc49fac1fe638c8bf8226399b1149
Pavel Hübner <pavel.hubner@hardwario.com>
2022-02-02 14:01:53 +0100

    scripts: Add RTT helper script
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

---
+++ scripts/rtt.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -eu
+
+touch ~/rtt.log
+
+tmux new-session \; \
+  bind-key -n C-q kill-session \; \
+  send-keys 'JLinkRTTLogger -Device NRF52840_XXAA -RTTChannel 1 -if SWD -Speed 4000 ~/rtt.log' C-m \; \
+  split-window -h -p 70 \; \
+  send-keys 'tail -f ~/rtt.log 2>/dev/null' C-m \; \
+  select-pane -t 0 \; \
+  split-window -v -p 70 \; \
+  send-keys 'while true; do nc localhost 19021; sleep 0.5; done' C-m
