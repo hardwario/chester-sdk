6b1dc2a3f0a8729514e04c90781be26bdb89ffc7
Pavel Hübner <pavel.hubner@hardwario.com>
2022-02-20 15:25:30 +0100

    ctr_rtc: Make subsystem + refactor
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

--- subsys/ctr_rtc/ctr_rtc.c
+++ subsys/ctr_rtc/ctr_rtc.c
@@ -1,3 +1,4 @@
+/* CHESTER includes */
 #include <ctr_rtc.h>
 
 /* Zephyr includes */
@@ -13,10 +14,11 @@
 
 /* Standard includes */
 #include <ctype.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
-LOG_MODULE_REGISTER(ctr_rtc, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(ctr_rtc, CONFIG_CTR_RTC_LOG_LEVEL);
 
 static const nrfx_rtc_t m_rtc = NRFX_RTC_INSTANCE(2);
 static struct onoff_client m_lfclk_cli;
@@ -134,10 +136,8 @@ static int cmd_rtc_get(const struct shell *shell, size_t argc, char **argv)
 	int ret;
 
 	struct ctr_rtc_tm tm;
-
 	ret = ctr_rtc_get_tm(&tm);
-
-	if (ret < 0) {
+	if (ret) {
 		LOG_ERR("Call `ctr_rtc_get_tm` failed: %d", ret);
 		return ret;
 	}
@@ -191,8 +191,7 @@ static int cmd_rtc_set(const struct shell *shell, size_t argc, char **argv)
 	tm.seconds = atoi(&time[6]);
 
 	ret = ctr_rtc_set_tm(&tm);
-
-	if (ret < 0) {
+	if (ret) {
 		LOG_ERR("Call `ctr_rtc_set_tm` failed: %d", ret);
 		return ret;
 	}
@@ -213,18 +212,25 @@ static int print_help(const struct shell *shell, size_t argc, char **argv)
 	return 0;
 }
 
-SHELL_STATIC_SUBCMD_SET_CREATE(sub_rtc,
-                               SHELL_CMD_ARG(get, NULL,
-                                             "Get current date/time"
-                                             " (format YYYY/MM/DD hh:mm:ss).",
-                                             cmd_rtc_get, 1, 0),
-                               SHELL_CMD_ARG(set, NULL,
-                                             "Set current date/time"
-                                             " (format YYYY/MM/DD hh:mm:ss).",
-                                             cmd_rtc_set, 3, 0),
-                               SHELL_SUBCMD_SET_END);
+/* clang-format off */
+
+SHELL_STATIC_SUBCMD_SET_CREATE(
+	sub_rtc,
+
+	SHELL_CMD_ARG(get, NULL,
+	              "Get current date/time (format YYYY/MM/DD hh:mm:ss).",
+	              cmd_rtc_get, 1, 0),
+
+	SHELL_CMD_ARG(set, NULL,
+	              "Set current date/time (format YYYY/MM/DD hh:mm:ss).",
+	              cmd_rtc_set, 3, 0),
+
+	SHELL_SUBCMD_SET_END
+);
 
 SHELL_CMD_REGISTER(rtc, &sub_rtc, "RTC commands for date/time operations.", print_help);
+
+/* clang-format on */
 
 static void rtc_handler(nrfx_rtc_int_type_t int_type)
 {
@@ -268,19 +274,16 @@ static int request_lfclk(void)
 	int ret;
 
 	struct onoff_manager *mgr = z_nrf_clock_control_get_onoff(CLOCK_CONTROL_NRF_SUBSYS_LF);
-
 	if (mgr == NULL) {
 		LOG_ERR("Call `z_nrf_clock_control_get_onoff` failed");
 		return -ENXIO;
 	}
 
 	static struct k_poll_signal sig;
-
 	k_poll_signal_init(&sig);
 	sys_notify_init_signal(&m_lfclk_cli.notify, &sig);
 
 	ret = onoff_request(mgr, &m_lfclk_cli);
-
 	if (ret < 0) {
 		LOG_ERR("Call `onoff_request` failed: %d", ret);
 		return ret;
@@ -289,10 +292,8 @@ static int request_lfclk(void)
 	struct k_poll_event events[] = {
 		K_POLL_EVENT_INITIALIZER(K_POLL_TYPE_SIGNAL, K_POLL_MODE_NOTIFY_ONLY, &sig),
 	};
-
 	ret = k_poll(events, ARRAY_SIZE(events), K_FOREVER);
-
-	if (ret < 0) {
+	if (ret) {
 		LOG_ERR("Call `k_poll` failed: %d", ret);
 		return ret;
 	}
@@ -309,17 +310,14 @@ static int init(const struct device *dev)
 	LOG_INF("System initialization");
 
 	ret = request_lfclk();
-
-	if (ret < 0) {
+	if (ret) {
 		LOG_ERR("Call `request_lfclk` failed: %d", ret);
 		return ret;
 	}
 
 	nrfx_rtc_config_t config = NRFX_RTC_DEFAULT_CONFIG;
 	config.prescaler = 4095;
-
 	nrfx_err_t err = nrfx_rtc_init(&m_rtc, &config, rtc_handler);
-
 	if (err != NRFX_SUCCESS) {
 		LOG_ERR("Call `nrfx_rtc_init` failed: %d", (int)err);
 		return -EIO;
