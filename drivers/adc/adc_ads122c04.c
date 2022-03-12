0b0ac77fb04e267de7d0ca309c7699feb808a163
Pavel Hübner <pavel.hubner@hardwario.com>
2022-03-12 16:38:44 +0100

    drivers: ads122c04: Add DT properties
    New DT properties have been added for build-time configuration:
    - gain
    - mux
    - vref
    
    These parameters will be preferrably runtime adjustable in the future,
    however this way we can immediately offer driver for different
    uses-cases.
    
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

--- drivers/adc/adc_ads122c04.c
+++ drivers/adc/adc_ads122c04.c
@@ -23,47 +23,57 @@ LOG_MODULE_REGISTER(ads122c04, CONFIG_ADC_LOG_LEVEL);
 #define REG_CONFIG_2 0x02
 #define REG_CONFIG_3 0x03
 
-#define CONFIG_0_PGA_BYPASS_pos 0
-#define CONFIG_0_PGA_BYPASS_msk BIT(0)
-#define CONFIG_0_GAIN_pos 1
-#define CONFIG_0_GAIN_msk BIT(1) | BIT(2) | BIT(3)
-#define CONFIG_0_MUX_pos 4
-#define CONFIG_0_MUX_msk BIT(4) | BIT(5) | BIT(6) | BIT(7)
+#define DEF_CONFIG_0 0x00
+#define DEF_CONFIG_1 0x00
+#define DEF_CONFIG_2 0x00
+#define DEF_CONFIG_3 0x00
 
-#define CONFIG_1_TS_pos 0
-#define CONFIG_1_TS_msk BIT(0)
-#define CONFIG_1_VREF_pos 1
-#define CONFIG_1_VREF_msk BIT(1) | BIT(2)
-#define CONFIG_1_CM_pos 3
-#define CONFIG_1_CM_msk BIT(3)
-#define CONFIG_1_MODE_pos 4
-#define CONFIG_1_MODE_msk BIT(4)
-#define CONFIG_1_DR_pos 5
-#define CONFIG_1_DR_msk BIT(5) | BIT(6) | BIT(7)
+#define POS_CONFIG_0_PGA_BYPASS 0
+#define POS_CONFIG_0_GAIN 1
+#define POS_CONFIG_0_MUX 4
+#define POS_CONFIG_1_TS 0
+#define POS_CONFIG_1_VREF 1
+#define POS_CONFIG_1_CM 3
+#define POS_CONFIG_1_MODE 4
+#define POS_CONFIG_1_DR 5
+#define POS_CONFIG_2_IDAC 0
+#define POS_CONFIG_2_BCS 3
+#define POS_CONFIG_2_CRC 4
+#define POS_CONFIG_2_DCNT 6
+#define POS_CONFIG_2_DRDY 7
+#define POS_CONFIG_3_I2MUX 2
+#define POS_CONFIG_3_I1MUX 5
 
-#define CONFIG_2_IDAC_pos 0
-#define CONFIG_2_IDAC_msk BIT(0) | BIT(1) | BIT(2)
-#define CONFIG_2_BCS_pos 3
-#define CONFIG_2_BCS_msk BIT(3)
-#define CONFIG_2_CRC_pos 4
-#define CONFIG_2_CRC_msk BIT(4) | BIT(5)
-#define CONFIG_2_DCNT_pos 6
-#define CONFIG_2_DCNT_msk BIT(6)
-#define CONFIG_2_DRDY_pos 7
-#define CONFIG_2_DRDY_msk BIT(7)
+#define MSK_CONFIG_0_MUX (BIT(4) | BIT(5) | BIT(6) | BIT(7))
+#define MSK_CONFIG_0_PGA_BYPASS BIT(0)
+#define MSK_CONFIG_0_GAIN (BIT(1) | BIT(2) | BIT(3))
+#define MSK_CONFIG_1_TS BIT(0)
+#define MSK_CONFIG_1_VREF (BIT(1) | BIT(2))
+#define MSK_CONFIG_1_CM BIT(3)
+#define MSK_CONFIG_1_MODE BIT(4)
+#define MSK_CONFIG_1_DR (BIT(5) | BIT(6) | BIT(7))
+#define MSK_CONFIG_2_IDAC (BIT(0) | BIT(1) | BIT(2))
+#define MSK_CONFIG_2_BCS BIT(3)
+#define MSK_CONFIG_2_CRC (BIT(4) | BIT(5))
+#define MSK_CONFIG_2_DCNT BIT(6)
+#define MSK_CONFIG_2_DRDY BIT(7)
+#define MSK_CONFIG_3_I2MUX (BIT(2) | BIT(3) | BIT(4))
+#define MSK_CONFIG_3_I1MUX (BIT(5) | BIT(6) | BIT(7))
 
-#define CONFIG_2_I2MUX_pos 2
-#define CONFIG_2_I2MUX_msk BIT(2) | BIT(3) | BIT(4)
-#define CONFIG_2_I1MUX_pos 5
-#define CONFIG_2_I1MUX_msk BIT(5) | BIT(6) | BIT(7)
-
-#define RREG_ADDR_SHIFT 2
+#define READ_MAX_RETRIES 100
 
 struct ads122c04_config {
 	const struct i2c_dt_spec i2c_spec;
+	int gain;
+	int mux;
+	int vref;
 };
 
 struct ads122c04_data {
+	uint8_t reg_config_0;
+	uint8_t reg_config_1;
+	uint8_t reg_config_2;
+	uint8_t reg_config_3;
 };
 
 static inline const struct ads122c04_config *get_config(const struct device *dev)
@@ -146,19 +156,21 @@ static inline int powerdown(const struct device *dev)
 	return write(dev, CMD_POWERDOWN, NULL, 0);
 }
 
-static inline int rdata(const struct device *dev, uint8_t *data, uint8_t len)
+static inline int rdata(const struct device *dev, uint8_t *val, uint8_t len)
 {
-	return read(dev, CMD_RDATA, data, len);
+	return read(dev, CMD_RDATA, val, len);
 }
 
-static inline int rreg(const struct device *dev, uint8_t addr, uint8_t *reg)
+static inline int rreg(const struct device *dev, uint8_t reg, uint8_t *val)
 {
-	return read(dev, CMD_RREG | (addr << RREG_ADDR_SHIFT), reg, 1);
+	return read(dev, CMD_RREG | reg << 2, val, 1);
 }
 
-static inline int wreg(const struct device *dev, uint8_t addr, uint8_t reg)
+static inline int wreg(const struct device *dev, uint8_t reg, uint8_t val)
 {
-	return write(dev, CMD_WREG | (addr << RREG_ADDR_SHIFT), &reg, 1);
+	LOG_DBG("Reg: 0x%02x = 0x%02x", reg, val);
+
+	return write(dev, CMD_WREG | reg << 2, &val, 1);
 }
 
 static int ads122c04_channel_setup(const struct device *dev,
@@ -179,14 +191,27 @@ static int ads122c04_read(const struct device *dev, const struct adc_sequence *s
 		return ret;
 	}
 
-	uint8_t reg;
-	do {
-		ret = rreg(dev, REG_CONFIG_2, &reg);
+	int retries = 0;
+
+	for (;;) {
+		uint8_t val;
+		ret = rreg(dev, REG_CONFIG_2, &val);
 		if (ret) {
 			LOG_ERR("Call `rreg` failed: %d", ret);
 			return ret;
 		}
-	} while (!(reg & CONFIG_2_DRDY_msk));
+
+		if (val & MSK_CONFIG_2_DRDY) {
+			break;
+		}
+
+		if (++retries >= READ_MAX_RETRIES) {
+			LOG_ERR("Reached maximum DRDY poll attempts");
+			return -EIO;
+		}
+
+		k_msleep(10);
+	}
 
 	uint8_t buf[3] = { 0 };
 	ret = rdata(dev, buf, sizeof(buf));
@@ -218,25 +243,33 @@ static int ads122c04_init(const struct device *dev)
 		return ret;
 	}
 
-	ret = wreg(dev, REG_CONFIG_0, 0x30);
+	get_data(dev)->reg_config_0 = DEF_CONFIG_0;
+	get_data(dev)->reg_config_0 |= get_config(dev)->gain << POS_CONFIG_0_GAIN;
+	get_data(dev)->reg_config_0 |= get_config(dev)->mux << POS_CONFIG_0_MUX;
+	get_data(dev)->reg_config_1 = DEF_CONFIG_1;
+	get_data(dev)->reg_config_1 |= get_config(dev)->vref << POS_CONFIG_1_VREF;
+	get_data(dev)->reg_config_2 = DEF_CONFIG_2;
+	get_data(dev)->reg_config_3 = DEF_CONFIG_3;
+
+	ret = wreg(dev, REG_CONFIG_0, get_data(dev)->reg_config_0);
 	if (ret) {
 		LOG_ERR("Call `wreg` failed: %d", ret);
 		return ret;
 	}
 
-	ret = wreg(dev, REG_CONFIG_1, 0x02);
+	ret = wreg(dev, REG_CONFIG_1, get_data(dev)->reg_config_1);
 	if (ret) {
 		LOG_ERR("Call `wreg` failed: %d", ret);
 		return ret;
 	}
 
-	ret = wreg(dev, REG_CONFIG_2, 0x06);
+	ret = wreg(dev, REG_CONFIG_2, get_data(dev)->reg_config_2);
 	if (ret) {
 		LOG_ERR("Call `wreg` failed: %d", ret);
 		return ret;
 	}
 
-	ret = wreg(dev, REG_CONFIG_3, 0x80);
+	ret = wreg(dev, REG_CONFIG_3, get_data(dev)->reg_config_3);
 	if (ret) {
 		LOG_ERR("Call `wreg` failed: %d", ret);
 		return ret;
@@ -254,6 +287,9 @@ static const struct adc_driver_api ads122c04_driver_api = {
 #define ADS122C04_INIT(n)                                                                          \
 	static const struct ads122c04_config inst_##n##_config = {                                 \
 		.i2c_spec = I2C_DT_SPEC_INST_GET(n),                                               \
+		.gain = DT_INST_PROP(n, gain),                                                     \
+		.mux = DT_INST_PROP(n, mux),                                                       \
+		.vref = DT_INST_PROP(n, vref),                                                     \
 	};                                                                                         \
 	static struct ads122c04_data inst_##n##_data;                                              \
 	DEVICE_DT_INST_DEFINE(n, ads122c04_init, NULL, &inst_##n##_data, &inst_##n##_config,       \
