7c8d5256212a189c0885e928510bfec00c568089
Pavel Hübner <pavel.hubner@hardwario.com>
2022-04-04 20:31:05 +0200

    samples: ctr_ble_scan: Add simple scanner example
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

---
+++ samples/ctr_ble_scan/src/main.c
@@ -0,0 +1,46 @@
+/* Zephyr includes */
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <logging/log.h>
+#include <zephyr.h>
+
+LOG_MODULE_REGISTER(app, LOG_LEVEL_DBG);
+
+static void scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type,
+                    struct net_buf_simple *buf)
+{
+	int ret;
+
+	char s[32];
+	ret = bt_addr_le_to_str(addr, s, sizeof(s));
+	if (!ret) {
+		return;
+	}
+
+	LOG_INF("BDADDR: %s RSSI: %d", s, rssi);
+}
+
+void main(void)
+{
+	int ret;
+
+	LOG_INF("Build time: " __DATE__ " " __TIME__);
+
+	struct bt_le_scan_param scan_param = {
+		.type = BT_HCI_LE_SCAN_PASSIVE,
+		.options = BT_LE_SCAN_OPT_NONE,
+		.interval = BT_GAP_SCAN_FAST_INTERVAL,
+		.window = BT_GAP_SCAN_FAST_WINDOW,
+	};
+
+	ret = bt_le_scan_start(&scan_param, scan_cb);
+	if (ret) {
+		LOG_ERR("Call `bt_le_scan_start` failed: %d", ret);
+		k_oops();
+	}
+
+	for (;;) {
+		LOG_INF("Alive");
+		k_sleep(K_SECONDS(1));
+	}
+}
