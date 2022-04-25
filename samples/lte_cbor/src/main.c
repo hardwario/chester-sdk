242a6ae40492d7afb5bf4ef8a7a43e5ee65d855c
Pavel Hübner <pavel.hubner@hardwario.com>
2022-04-25 19:02:32 +0200

    samples: lte_cbor: Add CBOR encoder + LTE sample
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

---
+++ samples/lte_cbor/src/main.c
@@ -0,0 +1,512 @@
+#include "msg_key.h"
+
+/* CHESTER includes */
+#include <ctr_buf.h>
+#include <ctr_info.h>
+#include <ctr_lte.h>
+#include <ctr_rtc.h>
+#include <ctr_therm.h>
+
+/* Zephyr includes */
+#include <logging/log.h>
+#include <tinycbor/cbor.h>
+#include <tinycbor/cbor_buf_writer.h>
+#include <tinycrypt/constants.h>
+#include <tinycrypt/sha256.h>
+#include <zephyr.h>
+
+/* Standard includes */
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+static K_SEM_DEFINE(m_attach_sem, 0, 1);
+static K_MUTEX_DEFINE(m_lte_eval_mut);
+static struct ctr_lte_eval m_lte_eval;
+static bool m_lte_eval_valid;
+
+static void start(void)
+{
+	int ret;
+
+	ret = ctr_lte_start(NULL);
+	if (ret) {
+		LOG_ERR("Call `ctr_lte_start` failed: %d", ret);
+		k_oops();
+	}
+}
+
+static void attach(void)
+{
+	int ret;
+
+	ret = ctr_lte_attach(NULL);
+	if (ret) {
+		LOG_ERR("Call `ctr_lte_attach` failed: %d", ret);
+		k_oops();
+	}
+}
+
+static void lte_event_handler(enum ctr_lte_event event, union ctr_lte_event_data *data, void *param)
+{
+	ARG_UNUSED(param);
+
+	switch (event) {
+	case CTR_LTE_EVENT_FAILURE:
+		LOG_ERR("Event `CTR_LTE_EVENT_FAILURE`");
+		start();
+		break;
+
+	case CTR_LTE_EVENT_START_OK:
+		LOG_INF("Event `CTR_LTE_EVENT_START_OK`");
+		attach();
+		break;
+
+	case CTR_LTE_EVENT_START_ERR:
+		LOG_ERR("Event `CTR_LTE_EVENT_START_ERR`");
+		start();
+		break;
+
+	case CTR_LTE_EVENT_ATTACH_OK:
+		LOG_INF("Event `CTR_LTE_EVENT_ATTACH_OK`");
+		k_sem_give(&m_attach_sem);
+		break;
+
+	case CTR_LTE_EVENT_ATTACH_ERR:
+		LOG_ERR("Event `CTR_LTE_EVENT_ATTACH_ERR`");
+		start();
+		break;
+
+	case CTR_LTE_EVENT_DETACH_OK:
+		LOG_INF("Event `CTR_LTE_EVENT_DETACH_OK`");
+		break;
+
+	case CTR_LTE_EVENT_DETACH_ERR:
+		LOG_ERR("Event `CTR_LTE_EVENT_DETACH_ERR`");
+		start();
+		break;
+
+	case CTR_LTE_EVENT_EVAL_OK:
+		LOG_INF("Event `CTR_LTE_EVENT_EVAL_OK`");
+
+		struct ctr_lte_eval *eval = &data->eval_ok.eval;
+
+		LOG_DBG("EEST: %d", eval->eest);
+		LOG_DBG("ECL: %d", eval->ecl);
+		LOG_DBG("RSRP: %d", eval->rsrp);
+		LOG_DBG("RSRQ: %d", eval->rsrq);
+		LOG_DBG("SNR: %d", eval->snr);
+		LOG_DBG("PLMN: %d", eval->plmn);
+		LOG_DBG("CID: %d", eval->cid);
+		LOG_DBG("BAND: %d", eval->band);
+		LOG_DBG("EARFCN: %d", eval->earfcn);
+
+		k_mutex_lock(&m_lte_eval_mut, K_FOREVER);
+		memcpy(&m_lte_eval, &data->eval_ok.eval, sizeof(m_lte_eval));
+		m_lte_eval_valid = true;
+		k_mutex_unlock(&m_lte_eval_mut);
+
+		break;
+
+	case CTR_LTE_EVENT_EVAL_ERR:
+		LOG_ERR("Event `CTR_LTE_EVENT_EVAL_ERR`");
+
+		k_mutex_lock(&m_lte_eval_mut, K_FOREVER);
+		m_lte_eval_valid = false;
+		k_mutex_unlock(&m_lte_eval_mut);
+
+		break;
+
+	case CTR_LTE_EVENT_SEND_OK:
+		LOG_INF("Event `CTR_LTE_EVENT_SEND_OK`");
+		break;
+
+	case CTR_LTE_EVENT_SEND_ERR:
+		LOG_ERR("Event `CTR_LTE_EVENT_SEND_ERR`");
+		start();
+		break;
+
+	default:
+		LOG_WRN("Unknown event: %d", event);
+		return;
+	}
+}
+
+int encode(CborEncoder *enc)
+{
+	int ret;
+
+	CborError err = 0;
+
+	CborEncoder map;
+	err |= cbor_encoder_create_map(enc, &map, CborIndefiniteLength);
+
+	err |= cbor_encode_uint(&map, MSG_KEY_FRAME);
+	{
+		CborEncoder map;
+		err |= cbor_encoder_create_map(enc, &map, CborIndefiniteLength);
+
+		uint8_t protocol = 3;
+		err |= cbor_encode_uint(&map, MSG_KEY_PROTOCOL);
+		err |= cbor_encode_uint(&map, protocol);
+
+		static uint32_t sequence;
+		err |= cbor_encode_uint(&map, MSG_KEY_SEQUENCE);
+		err |= cbor_encode_uint(&map, sequence++);
+
+		uint64_t timestamp;
+		ret = ctr_rtc_get_ts(&timestamp);
+		if (ret) {
+			LOG_ERR("Call `ctr_rtc_get_ts` failed: %d", ret);
+			return ret;
+		}
+
+		err |= cbor_encode_uint(&map, MSG_KEY_TIMESTAMP);
+		err |= cbor_encode_uint(&map, timestamp);
+
+		err |= cbor_encoder_close_container(enc, &map);
+	}
+
+	err |= cbor_encode_uint(&map, MSG_KEY_ATTRIBUTE);
+	{
+		CborEncoder map;
+		err |= cbor_encoder_create_map(enc, &map, CborIndefiniteLength);
+
+		char *vendor_name;
+		ctr_info_get_vendor_name(&vendor_name);
+
+		err |= cbor_encode_uint(&map, MSG_KEY_VENDOR_NAME);
+		err |= cbor_encode_text_stringz(&map, vendor_name);
+
+		char *product_name;
+		ctr_info_get_product_name(&product_name);
+
+		err |= cbor_encode_uint(&map, MSG_KEY_PRODUCT_NAME);
+		err |= cbor_encode_text_stringz(&map, product_name);
+
+		char *hw_variant;
+		ctr_info_get_hw_variant(&hw_variant);
+
+		err |= cbor_encode_uint(&map, MSG_KEY_HW_VARIANT);
+		err |= cbor_encode_text_stringz(&map, hw_variant);
+
+		char *hw_revision;
+		ctr_info_get_hw_revision(&hw_revision);
+
+		err |= cbor_encode_uint(&map, MSG_KEY_HW_REVISION);
+		err |= cbor_encode_text_stringz(&map, hw_revision);
+
+		char *fw_version;
+		ctr_info_get_fw_version(&fw_version);
+
+		err |= cbor_encode_uint(&map, MSG_KEY_FW_VERSION);
+		err |= cbor_encode_text_stringz(&map, fw_version);
+
+		char *serial_number;
+		ctr_info_get_serial_number(&serial_number);
+
+		err |= cbor_encode_uint(&map, MSG_KEY_SERIAL_NUMBER);
+		err |= cbor_encode_text_stringz(&map, serial_number);
+
+		err |= cbor_encoder_close_container(enc, &map);
+	}
+
+	err |= cbor_encode_uint(&map, MSG_KEY_STATE);
+	{
+		CborEncoder map;
+		err |= cbor_encoder_create_map(enc, &map, CborIndefiniteLength);
+
+		err |= cbor_encode_uint(&map, MSG_KEY_UPTIME);
+		err |= cbor_encode_uint(&map, k_uptime_get() / 1000);
+
+		err |= cbor_encoder_close_container(enc, &map);
+	}
+
+	err |= cbor_encode_uint(&map, MSG_KEY_NETWORK);
+	{
+		CborEncoder map;
+		err |= cbor_encoder_create_map(enc, &map, CborIndefiniteLength);
+
+		uint64_t imei;
+		ret = ctr_lte_get_imei(&imei);
+		if (ret) {
+			LOG_ERR("Call `ctr_lte_get_imei` failed: %d", ret);
+			return ret;
+		}
+
+		err |= cbor_encode_uint(&map, MSG_KEY_IMEI);
+		err |= cbor_encode_uint(&map, imei);
+
+		uint64_t imsi;
+		ret = ctr_lte_get_imsi(&imsi);
+		if (ret) {
+			LOG_ERR("Call `ctr_lte_get_imsi` failed: %d", ret);
+			return ret;
+		}
+
+		err |= cbor_encode_uint(&map, MSG_KEY_IMSI);
+		err |= cbor_encode_uint(&map, imsi);
+
+		err |= cbor_encode_uint(&map, MSG_KEY_PARAMETER);
+		{
+			CborEncoder map;
+			err |= cbor_encoder_create_map(enc, &map, CborIndefiniteLength);
+
+			k_mutex_lock(&m_lte_eval_mut, K_FOREVER);
+
+			err |= cbor_encode_uint(&map, MSG_KEY_EEST);
+			if (m_lte_eval_valid) {
+				err |= cbor_encode_int(&map, m_lte_eval.eest);
+			} else {
+				err |= cbor_encode_null(&map);
+			}
+
+			err |= cbor_encode_uint(&map, MSG_KEY_ECL);
+			if (m_lte_eval_valid) {
+				err |= cbor_encode_int(&map, m_lte_eval.ecl);
+			} else {
+				err |= cbor_encode_null(&map);
+			}
+
+			err |= cbor_encode_uint(&map, MSG_KEY_RSRP);
+			if (m_lte_eval_valid) {
+				err |= cbor_encode_int(&map, m_lte_eval.rsrp);
+			} else {
+				err |= cbor_encode_null(&map);
+			}
+
+			err |= cbor_encode_uint(&map, MSG_KEY_RSRQ);
+			if (m_lte_eval_valid) {
+				err |= cbor_encode_int(&map, m_lte_eval.rsrq);
+			} else {
+				err |= cbor_encode_null(&map);
+			}
+
+			err |= cbor_encode_uint(&map, MSG_KEY_SNR);
+			if (m_lte_eval_valid) {
+				err |= cbor_encode_int(&map, m_lte_eval.snr);
+			} else {
+				err |= cbor_encode_null(&map);
+			}
+
+			err |= cbor_encode_uint(&map, MSG_KEY_PLMN);
+			if (m_lte_eval_valid) {
+				err |= cbor_encode_int(&map, m_lte_eval.plmn);
+			} else {
+				err |= cbor_encode_null(&map);
+			}
+
+			err |= cbor_encode_uint(&map, MSG_KEY_CID);
+			if (m_lte_eval_valid) {
+				err |= cbor_encode_int(&map, m_lte_eval.cid);
+			} else {
+				err |= cbor_encode_null(&map);
+			}
+
+			err |= cbor_encode_uint(&map, MSG_KEY_BAND);
+			if (m_lte_eval_valid) {
+				err |= cbor_encode_int(&map, m_lte_eval.band);
+			} else {
+				err |= cbor_encode_null(&map);
+			}
+
+			err |= cbor_encode_uint(&map, MSG_KEY_EARFCN);
+			if (m_lte_eval_valid) {
+				err |= cbor_encode_int(&map, m_lte_eval.earfcn);
+			} else {
+				err |= cbor_encode_null(&map);
+			}
+
+			m_lte_eval_valid = false;
+
+			k_mutex_unlock(&m_lte_eval_mut);
+
+			err |= cbor_encoder_close_container(enc, &map);
+		}
+
+		err |= cbor_encoder_close_container(enc, &map);
+	}
+
+	err |= cbor_encode_uint(&map, MSG_KEY_THERMOMETER);
+	{
+		CborEncoder map;
+		err |= cbor_encoder_create_map(enc, &map, CborIndefiniteLength);
+
+		float temperature;
+		ret = ctr_therm_read(&temperature);
+		if (ret) {
+			LOG_ERR("Call `ctr_therm_read` failed: %d", ret);
+			temperature = NAN;
+		}
+
+		err |= cbor_encode_uint(&map, MSG_KEY_TEMPERATURE);
+		if (isnan(temperature)) {
+			err |= cbor_encode_null(&map);
+		} else {
+			err |= cbor_encode_int(&map, temperature * 100.f);
+		}
+
+		err |= cbor_encoder_close_container(enc, &map);
+	}
+
+	err |= cbor_encoder_close_container(enc, &map);
+
+	return err ? -ENOSPC : 0;
+}
+
+static int compose(struct ctr_buf *buf)
+{
+	int ret;
+
+	ctr_buf_reset(buf);
+	ret = ctr_buf_seek(buf, 8);
+	if (ret) {
+		LOG_ERR("Call `ctr_buf_seek` failed: %d", ret);
+		return ret;
+	}
+
+	uint32_t serial_number;
+	ret = ctr_info_get_serial_number_uint32(&serial_number);
+	if (ret) {
+		LOG_ERR("Call `ctr_info_get_serial_number_uint32` failed: %d", ret);
+		return ret;
+	}
+
+	ret = ctr_buf_append_u32(buf, serial_number);
+	if (ret) {
+		LOG_ERR("Call `ctr_buf_append_u32` failed: %d", ret);
+		return ret;
+	}
+
+	struct cbor_buf_writer writer;
+	cbor_buf_writer_init(&writer, ctr_buf_get_mem(buf) + 12, ctr_buf_get_free(buf));
+
+	CborEncoder enc;
+	cbor_encoder_init(&enc, &writer.enc, 0);
+
+	ret = encode(&enc);
+	if (ret) {
+		LOG_ERR("Call `encode` failed: %d", ret);
+		return ret;
+	}
+
+	size_t len = 12 + enc.writer->bytes_written;
+
+	struct tc_sha256_state_struct s;
+	ret = tc_sha256_init(&s);
+	if (ret != TC_CRYPTO_SUCCESS) {
+		LOG_ERR("Call `tc_sha256_init` failed: %d", ret);
+		return ret;
+	}
+
+	ret = tc_sha256_update(&s, ctr_buf_get_mem(buf) + 8, len - 8);
+	if (ret != TC_CRYPTO_SUCCESS) {
+		LOG_ERR("Call `tc_sha256_update` failed: %d", ret);
+		return ret;
+	}
+
+	uint8_t digest[32];
+	ret = tc_sha256_final(digest, &s);
+	if (ret != TC_CRYPTO_SUCCESS) {
+		LOG_ERR("Call `tc_sha256_final` failed: %d", ret);
+		return ret;
+	}
+
+	ret = ctr_buf_seek(buf, 0);
+	if (ret) {
+		LOG_ERR("Call `ctr_buf_seek` failed: %d", ret);
+		return ret;
+	}
+
+	ret = ctr_buf_append_u16(buf, len);
+	if (ret) {
+		LOG_ERR("Call `ctr_buf_append_u16` failed: %d", ret);
+		return ret;
+	}
+
+	ret = ctr_buf_append_mem(buf, digest, 6);
+	if (ret) {
+		LOG_ERR("Call `ctr_buf_append_mem` failed: %d", ret);
+		return ret;
+	}
+
+	ret = ctr_buf_seek(buf, len);
+	if (ret) {
+		LOG_ERR("Call `ctr_buf_seek` failed: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int send(void)
+{
+	int ret;
+
+	CTR_BUF_DEFINE_STATIC(buf, 512);
+
+	ret = compose(&buf);
+	if (ret) {
+		LOG_ERR("Call `compose` failed: %d", ret);
+		return ret;
+	}
+
+	ret = ctr_lte_eval(NULL);
+	if (ret) {
+		LOG_ERR("Call `ctr_lte_eval` failed: %d", ret);
+		return ret;
+	}
+
+	struct ctr_lte_send_opts opts = CTR_LTE_SEND_OPTS_DEFAULTS;
+	opts.port = 5000;
+	ret = ctr_lte_send(&opts, ctr_buf_get_mem(&buf), ctr_buf_get_used(&buf), NULL);
+	if (ret) {
+		LOG_ERR("Call `ctr_lte_send` failed: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+void main(void)
+{
+	int ret;
+
+	ret = ctr_therm_init();
+	if (ret) {
+		LOG_ERR("Call `ctr_therm_init` failed: %d", ret);
+		k_oops();
+	}
+
+	ret = ctr_lte_set_event_cb(lte_event_handler, NULL);
+	if (ret) {
+		LOG_ERR("Call `ctr_lte_set_event_cb` failed: %d", ret);
+		k_oops();
+	}
+
+	ret = ctr_lte_start(NULL);
+	if (ret) {
+		LOG_ERR("Call `ctr_lte_start` failed: %d", ret);
+		k_oops();
+	}
+
+	ret = k_sem_take(&m_attach_sem, K_FOREVER);
+	if (ret) {
+		LOG_ERR("Call `k_sem_take` failed: %d", ret);
+		k_oops();
+	}
+
+	for (;;) {
+		LOG_INF("Alive");
+
+		ret = send();
+		if (ret) {
+			LOG_ERR("Call `send` failed: %d", ret);
+		}
+
+		k_sleep(K_SECONDS(60));
+	}
+}
