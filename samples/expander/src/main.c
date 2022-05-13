6ac6ec6ba8387ab8aee8cfe6a27c6b7748a5b2e4
Martin Hubáček <martin.hubacek@hardwario.com>
2022-05-13 15:13:33 +0200

    samples: expander: Add GPIO expander sample
    Signed-off-by: Martin Hubáček <martin.hubacek@hardwario.com>

---
+++ samples/expander/src/main.c
@@ -0,0 +1,55 @@
+/* CHESTER includes */
+#include <ctr_led.h>
+
+/* Zephyr includes */
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+#include <logging/log.h>
+#include <zephyr.h>
+
+/* Standard includes */
+#include <stdbool.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+static const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(ctr_b1_tca9534a));
+
+void main(void)
+{
+	int ret;
+
+	LOG_INF("Build time: " __DATE__ " " __TIME__);
+
+	bool state = false;
+
+	if (!device_is_ready(dev)) {
+		LOG_ERR("Device not ready");
+		k_oops();
+	}
+
+	ret = gpio_pin_configure(dev, 5, GPIO_OUTPUT | GPIO_ACTIVE_LOW);
+	if (ret) {
+		LOG_ERR("Call `gpio_pin_configure` failed: %d", ret);
+		k_oops();
+	}
+
+	for (;;) {
+		LOG_INF("Alive");
+
+		/* Invert state variable */
+		state = !state;
+
+		/* Control LED */
+		ctr_led_set(CTR_LED_CHANNEL_R, state);
+
+		ret = gpio_pin_set(dev, 5, state ? 1 : 0);
+		if (ret) {
+			LOG_ERR("Call `gpio_pin_set` failed: %d", ret);
+			k_oops();
+		}
+
+		/* Wait 500 ms */
+		k_sleep(K_MSEC(500));
+	}
+}
