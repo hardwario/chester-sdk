9f955b94789b09c90d77ec330e7d5c42d3832ccb
Pavel Hübner <pavel.hubner@hardwario.com>
2022-09-29 12:14:58 +0200

    samples: sleep_chester_x2: Add sample
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

---
+++ samples/sleep_chester_x2/src/main.c
@@ -0,0 +1,41 @@
+/* CHESTER includes */
+#include <chester/ctr_led.h>
+
+/* Zephyr includes */
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/pm/device.h>
+#include <zephyr/zephyr.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+void main(void)
+{
+	int ret;
+
+	LOG_INF("Build time: " __DATE__ " " __TIME__);
+
+	static const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(ctr_x2_sc16is740_a));
+
+	if (!device_is_ready(dev)) {
+		LOG_ERR("Device not ready");
+		k_oops();
+	}
+
+	ret = pm_device_action_run(dev, PM_DEVICE_ACTION_SUSPEND);
+	if (ret && ret != -EALREADY) {
+		LOG_ERR("Call `pm_device_action_run` failed: %d", ret);
+		k_oops();
+	}
+
+	for (;;) {
+		LOG_INF("Alive");
+
+		ctr_led_set(CTR_LED_CHANNEL_R, true);
+		k_sleep(K_MSEC(50));
+		ctr_led_set(CTR_LED_CHANNEL_R, false);
+
+		k_sleep(K_SECONDS(10));
+	}
+}
