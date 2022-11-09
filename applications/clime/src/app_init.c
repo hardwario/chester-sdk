8194ee0ec29861cd1d169b97b0143d65598b8cd5
Pavel Hübner <pavel.hubner@hardwario.com>
2022-11-09 13:45:42 +0100

    application: clime: Add CHESTER-Z support
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

--- applications/clime/src/app_init.c
+++ applications/clime/src/app_init.c
@@ -10,6 +10,7 @@
 #include <chester/ctr_lte.h>
 #include <chester/ctr_wdog.h>
 #include <chester/drivers/ctr_s1.h>
+#include <chester/drivers/ctr_z.h>
 
 /* Zephyr includes */
 #include <zephyr/device.h>
@@ -31,6 +32,28 @@ K_SEM_DEFINE(g_app_init_sem, 0, 1);
 #endif /* defined(CONFIG_SHIELD_CTR_LTE) */
 
 struct ctr_wdog_channel g_app_wdog_channel;
+
+#if defined(CONFIG_SHIELD_CTR_Z)
+static int init_ctr_z(void)
+{
+	int ret;
+
+	static const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(ctr_z));
+
+	if (!device_is_ready(dev)) {
+		LOG_ERR("Device not ready");
+		return -ENODEV;
+	}
+
+	ret = ctr_z_enable_interrupts(dev);
+	if (ret) {
+		LOG_ERR("Call `ctr_z_enable_interrupts` failed: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+#endif /* defined(CONFIG_SHIELD_CTR_Z) */
 
 int app_init(void)
 {
@@ -55,6 +78,14 @@ int app_init(void)
 		LOG_ERR("Call `ctr_wdog_start` failed: %d", ret);
 		return ret;
 	}
+
+#if defined(CONFIG_SHIELD_CTR_Z)
+	ret = init_ctr_z();
+	if (ret) {
+		LOG_ERR("Call `init_ctr_z` failed: %d", ret);
+		return ret;
+	}
+#endif /* defined(CONFIG_SHIELD_CTR_Z) */
 
 #if defined(CONFIG_SHIELD_CTR_DS18B20)
 	ret = ctr_ds18b20_scan();
