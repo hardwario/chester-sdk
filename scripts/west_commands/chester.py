1aa29a9e4dde75f54668a85c25316cd2f69d576a
Pavel Hübner <pavel.hubner@hardwario.com>
2022-12-15 09:57:37 +0400

    west: Move UUID generator for BLE to West

---
+++ scripts/west_commands/chester.py
@@ -0,0 +1,41 @@
+'''chester.py
+
+West extension for HARDWARIO CHESTER platform.'''
+
+import uuid
+from textwrap import dedent
+
+from west import log
+from west.commands import WestCommand
+
+
+class ChesterGenerateBluetoothUuid(WestCommand):
+
+    def __init__(self):
+        super().__init__(
+            'chester-gen-bt-uuid',
+            'generate Bluetooth UUID',
+            dedent('''
+            This helper command generate Bluetooth UUID for GATT service
+            or GATT characteristic, and can be copy-pasted to the object
+            definition.'''))
+
+    def do_add_parser(self, parser_adder):
+        parser = parser_adder.add_parser(self.name,
+                                         help=self.help,
+                                         description=self.description)
+        return parser
+
+    def do_run(self, args, unknown_args):
+        u = str(uuid.uuid4())
+
+        log.inf('Generated 128-bit UUID:')
+        log.inf(u)
+        log.inf()
+
+        u = u.split('-')
+        u = f'0x{u[0]}, 0x{u[1]}, 0x{u[2]}, 0x{u[3]}, 0x{u[4]}'
+
+        log.inf('Definition for the Zephyr RTOS:')
+        log.inf('static struct bt_uuid_128 uuid = ' +
+                'BT_UUID_INIT_128(BT_UUID_128_ENCODE(' + u + '));')
