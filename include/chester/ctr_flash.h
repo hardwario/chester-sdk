1c18a101183c927512a90d560678190621b4d59a
Pavel Hübner <pavel.hubner@hardwario.com>
2023-01-22 07:49:10 +0100

    subsys: ctr_flash: Add external flash subsystem
    Enabling CTR_FLASH=y in prj.conf reduces application power consumption,
    since it suspends the NOR flash from the idle mode.
    
    The sleep sample has been updated. Current measurement without the LTE / LoRaWAN
    modules assembled, without DC/DC converter, supercapacitors + supercapacitor charger,
    shows 24 uA (without J-Link connected).
    
    This is the break-down of the typical chip power
    consumption in low-power state:
    
    - nRF52840: 7.4 uA (calculated as difference)
    - TPS7A20: 6.5 uA
    - LIS2DH12: 4 uA (ODR 10 Hz)
    - DS2484: 3 uA
    - TMP112: 2 uA (ODR 0.25 Hz)
    - TLA2021: 0.5 uA
    - AT45DB641E: 0.4 uA
    - SHT30: 0.2 uA
    
    Most figures above are taken from datasheet as typical values,
    but from experience, these pretty much correspond to the reality.
    
    Signed-off-by: Pavel Hübner <pavel.hubner@hardwario.com>

---
+++ include/chester/ctr_flash.h
@@ -0,0 +1,15 @@
+#ifndef CHESTER_INCLUDE_CTR_FLASH_H_
+#define CHESTER_INCLUDE_CTR_FLASH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int ctr_flash_acquire(void);
+int ctr_flash_release(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CHESTER_INCLUDE_CTR_FLASH_H_ */
---
+++ subsys/ctr_flash/ctr_flash.c
@@ -0,0 +1,110 @@
+/* CHESTER includes */
+#include <chester/ctr_flash.h>
+
+/* Zephyr includes */
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/pm/device.h>
+
+/* Standard includes */
+#include <errno.h>
+
+LOG_MODULE_REGISTER(ctr_flash, CONFIG_CTR_FLASH_LOG_LEVEL);
+
+static K_MUTEX_DEFINE(m_lock);
+
+static int suspend(void)
+{
+	int ret;
+
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(flash1));
+	if (!device_is_ready(dev)) {
+		LOG_ERR("Device not ready");
+		return -ENODEV;
+	}
+
+	ret = pm_device_action_run(dev, PM_DEVICE_ACTION_SUSPEND);
+	if (ret && ret != -EALREADY) {
+		LOG_ERR("Call `pm_device_action_run` failed: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int resume(void)
+{
+	int ret;
+
+	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(flash1));
+	if (!device_is_ready(dev)) {
+		LOG_ERR("Device not ready");
+		return -ENODEV;
+	}
+
+	ret = pm_device_action_run(dev, PM_DEVICE_ACTION_RESUME);
+	if (ret && ret != -EALREADY) {
+		LOG_ERR("Call `pm_device_action_run` failed: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+int ctr_flash_acquire(void)
+{
+	int ret;
+
+	ret = k_mutex_lock(&m_lock, K_NO_WAIT);
+	if (ret) {
+		LOG_ERR("Call `k_mutex_lock` failed: %d", ret);
+		return ret;
+	}
+
+	ret = resume();
+	if (ret) {
+		LOG_ERR("Call `resume` failed: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+int ctr_flash_release(void)
+{
+	int ret;
+
+	ret = suspend();
+	if (ret) {
+		LOG_ERR("Call `suspend` failed: %d", ret);
+		return ret;
+	}
+
+	ret = k_mutex_unlock(&m_lock);
+	if (ret) {
+		LOG_ERR("Call `k_mutex_unlock` failed: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int init(const struct device *dev)
+{
+	int ret;
+
+	LOG_INF("System initialization");
+
+	ret = suspend();
+	if (ret) {
+		LOG_ERR("Call `suspend` failed: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+SYS_INIT(init, APPLICATION, CONFIG_CTR_FLASH_INIT_PRIORITY);
