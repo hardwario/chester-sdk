1acb7bffa10e5ecc4962f000cb93bb7973d51d19
fyziktom <tomas.svoboda@technicinsider.com>
2024-06-13 08:52:19 -0400

    update decoders for IAQ, add decoder for Current

--- applications/clime/codec/cs-decoder.js
+++ applications/clime/codec/cs-decoder.js
@@ -2,11 +2,12 @@ var cursor = 0;
 var buffer;
 
 // Uncomment for ChirpStack:
-function Decode(port, bytes, variables) {
+function decodeUplink(input) {
 
 // Uncomment for The Things Network:
-// function Decoder(port, bytes) {
-
+// function decodeUplink(port, bytes) {
+    var bytes = input.bytes;
+    cursor = 0;
     buffer = bytes;
 
     var data = {};
@@ -15,7 +16,7 @@ function Decode(port, bytes, variables) {
 
     if ((header & 0x01) !== 0) {
         data.voltage_rest = u16();
-        data.voltage_load =  u16();
+        data.voltage_load = u16();
         data.current_load = u8();
 
         if (data.voltage_rest === 0xffff) {
@@ -44,13 +45,55 @@ function Decode(port, bytes, variables) {
     }
 
     if ((header & 0x04) !== 0) {
-        data.therm_temperature = s16();
+        data.internal_temperature = s16();
+        if (data.internal_temperature === 0x7fff) {
+            data.internal_temperature = null;
+        } else {
+            data.internal_temperature = data.internal_temperature / 100;
+        }
 
+        data.therm_temperature = s16();
         if (data.therm_temperature === 0x7fff) {
             data.therm_temperature = null;
         } else {
             data.therm_temperature = data.therm_temperature / 100;
         }
+
+        data.hygro_humidity = u16();
+        if (data.hygro_humidity === 0xffff) {
+            data.hygro_humidity = null;
+        } else {
+            data.hygro_humidity = data.hygro_humidity / 100;
+        }
+
+        data.altitude = u16();
+        if (data.altitude === 0xffff) {
+            data.altitude = null;
+        } else {
+            data.altitude = data.altitude / 100;
+        }
+
+        data.co2 = u16();
+        if (data.co2 === 0xffff) {
+            data.co2 = null;
+        } else {
+            data.co2 = data.co2 / 100;
+        }
+
+        data.illuminance = u16();
+        if (data.illuminance === 0xffff) {
+            data.illuminance = null;
+        } else {
+            data.illuminance = data.illuminance / 100;
+        }
+      
+        data.presure = u16();
+        if (data.presure === 0xffff) {
+            data.presure = null;
+        } else {
+            data.presure = data.presure / 100;
+        }
+
     }
 
     if ((header & 0x10) !== 0) {
@@ -106,12 +149,21 @@ function Decode(port, bytes, variables) {
         }
     }
 
-    return data;
+    if ((header & 0x80) !== 0) {
+        var line = u8();
+        data.dc_line = (line !== 0);
+    }
+
+    return {
+        data: data
+    };
 }
 
 function s8() {
-    var value = buffer.slice(cursor);
-    value = value[0];
+    if (cursor >= buffer.length) {
+        throw new Error("Index out of range");
+    }
+    var value = buffer[cursor];
     if ((value & (1 << 7)) > 0) {
         value = (~value & 0xff) + 1;
         value = -value;
@@ -121,15 +173,19 @@ function s8() {
 }
 
 function u8() {
-    var value = buffer.slice(cursor);
-    value = value[0];
+    if (cursor >= buffer.length) {
+        throw new Error("Index out of range");
+    }
+    var value = buffer[cursor];
     cursor = cursor + 1;
     return value;
 }
 
 function s16() {
-    var value = buffer.slice(cursor);
-    value = value[0] | value[1] << 8;
+    if (cursor + 1 >= buffer.length) {
+        throw new Error("Index out of range");
+    }
+    var value = buffer[cursor] | (buffer[cursor + 1] << 8);
     if ((value & (1 << 15)) > 0) {
         value = (~value & 0xffff) + 1;
         value = -value;
@@ -139,22 +195,28 @@ function s16() {
 }
 
 function u16() {
-    var value = buffer.slice(cursor);
-    value = value[0] | value[1] << 8;
+    if (cursor + 1 >= buffer.length) {
+        throw new Error("Index out of range");
+    }
+    var value = buffer[cursor] | (buffer[cursor + 1] << 8);
     cursor = cursor + 2;
     return value;
 }
 
 function u32() {
-    var value = buffer.slice(cursor);
-    value = value[0] | value[1] << 8 | value[2] << 16 | value[3] << 24;
+    if (cursor + 3 >= buffer.length) {
+        throw new Error("Index out of range");
+    }
+    var value = buffer[cursor] | (buffer[cursor + 1] << 8) | (buffer[cursor + 2] << 16) | (buffer[cursor + 3] << 24);
     cursor = cursor + 4;
     return value;
 }
 
 function s32() {
-    var value = buffer.slice(cursor);
-    value = value[0] | value[1] << 8 | value[2] << 16 | value[3] << 24;
+    if (cursor + 3 >= buffer.length) {
+        throw new Error("Index out of range");
+    }
+    var value = buffer[cursor] | (buffer[cursor + 1] << 8) | (buffer[cursor + 2] << 16) | (buffer[cursor + 3] << 24);
     if ((value & (1 << 31)) > 0) {
         value = (~value & 0xffffffff) + 1;
         value = -value;
