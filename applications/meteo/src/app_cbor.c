a468cf904bcc586fe115560e6e6d592ec2ee38c1
Martin Hubáček <martin.hubacek@hardwario.com>
2024-11-12 11:48:33 +0100

    applications: meteo: Add temp. fix to not have '+++' in CBOR

--- applications/meteo/src/app_cbor.c
+++ applications/meteo/src/app_cbor.c
@@ -38,25 +38,43 @@ __unused static void put_sample_mul(zcbor_state_t *zs, struct app_data_aggreg *s
 	if (isnan(sample->min)) {
 		zcbor_nil_put(zs, NULL);
 	} else {
-		zcbor_int32_put(zs, sample->min * mul);
+		int32_t v = sample->min * mul;
+		// -12 -> -0.12°C * 100 -> 0x2B (character '+') - fix for AT modem terminal string
+		// ending '+++'
+		if (v == -12) {
+			v = -13;
+		}
+		zcbor_int32_put(zs, v);
 	}
 
 	if (isnan(sample->max)) {
 		zcbor_nil_put(zs, NULL);
 	} else {
-		zcbor_int32_put(zs, sample->max * mul);
+		int32_t v = sample->max * mul;
+		if (v == -12) {
+			v = -13;
+		}
+		zcbor_int32_put(zs, v);
 	}
 
 	if (isnan(sample->avg)) {
 		zcbor_nil_put(zs, NULL);
 	} else {
-		zcbor_int32_put(zs, sample->avg * mul);
+		int32_t v = sample->avg * mul;
+		if (v == -12) {
+			v = -13;
+		}
+		zcbor_int32_put(zs, v);
 	}
 
 	if (isnan(sample->mdn)) {
 		zcbor_nil_put(zs, NULL);
 	} else {
-		zcbor_int32_put(zs, sample->mdn * mul);
+		int32_t v = sample->mdn * mul;
+		if (v == -12) {
+			v = -13;
+		}
+		zcbor_int32_put(zs, v);
 	}
 }
 
