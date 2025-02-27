7c8ec35ef6ad4b2000c5a082ebd21d73a2dd19e3
Karel Blavka <karel.blavka@hardwario.com>
2025-02-27 11:39:25 +0100

    Refactor GitHub workflow: Per-app project.yaml for matrix builds
    Refactored the GitHub workflow logic for matrix builds. Instead of a single application.yml in the root directory, each application now defines its build variants in its own project.yaml. The old application.yml has been removed.
    
    Signed-off-by: Karel Blavka <karel.blavka@hardwario.com>

--- .github/gen-applications.py
+++ .github/gen-applications.py
@@ -6,68 +6,41 @@ import os
 
 PREFIX = 'com.hardwario.chester.app.'
 
+
+def panic(msg):
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+
 if len(sys.argv) != 3:
-    print("Usage: gen-applications.py <bundle> <type>")
-    sys.exit(1)
+    panic("Usage: gen-applications.py <app> <out>")
 
-bundle = sys.argv[1]
-if not bundle.startswith('com.'):
-    bundle = f'{PREFIX}{bundle}'
+app = sys.argv[1]
+out = sys.argv[2]
 
+if out not in ['path', 'cmd']:
+    panic("Unknown out type supported: path, cmd")
+
+fw_bundle = f'{PREFIX}{app}'
 path = os.path.dirname(os.path.realpath(__file__))
-filepath = os.path.join(path, '../applications/applications.yaml')
-applications = yaml.safe_load(open(filepath))
 
+name = app.split('.')[0]
 
-for a in applications:
-    if 'fw_bundle' in a and a['fw_bundle'] == bundle:
-        if sys.argv[2] == 'cmd':
+if out == 'path':
+    print(f'chester-sdk/chester/applications/{name}')
+    sys.exit(0)
 
-            cmd = f'FW_BUNDLE={a["fw_bundle"]}'
+project_yml_path = os.path.join(path, '../applications', name , 'project.yaml')
+if os.path.exists(project_yml_path):
+    project = yaml.safe_load(open(project_yml_path, 'r'))
+    for variant in project['variants']:
+        if variant['fw_bundle'] == fw_bundle:
+            # print(json.dumps(variant, sort_keys=True))
+            # sys.exit(0)
+            if out == 'cmd':
+                print(f'rm -rf build && west chester-update --variant "{variant["name"]}" && west build')
+                sys.exit(0)
 
-            if 'fw_name' in a:
-                cmd += f' FW_NAME="{a["fw_name"]}"'
-
-            if 'fw_version' in a:
-                cmd += f' FW_VERSION={a["fw_version"]}'
-
-            cmd += ' west build'
-
-            if 'shield' in a or 'extra_args' in a:
-                cmd += ' --'
-
-            if 'shield' in a:
-                cmd += f' -DSHIELD="{a["shield"]}"'
-
-            if 'extra_args' in a:
-                cmd += f' {a["extra_args"]}'
-
-            print(cmd)
-
-            sys.exit(0)
-
-        elif sys.argv[2] == 'path':
-
-            if not 'path' in a:
-                d = a['fw_bundle'].split('.')[4]
-            else:
-                d = a['path']
-
-            print(f'chester-sdk/chester/applications/{d}')
-
-            sys.exit(0)
-
-        elif sys.argv[2] == 'info':
-            print(json.dumps(a, sort_keys=True))
-            sys.exit(0)
-
-        elif sys.argv[2] == 'fw_bundle':
-            print(a['fw_bundle'])
-            sys.exit(0)
-
-        else:
-            print("Unknown type")
-            sys.exit(1)
+    panic(f'Variant not found: {fw_bundle}')
 else:
-    print("Bundle not found")
-    sys.exit(1)
+    panic(f'Project not found: {name}')
--- .github/gen-matrix.py
+++ .github/gen-matrix.py
@@ -5,23 +5,37 @@ import json
 import os
 
 PREFIX = 'com.hardwario.chester.app.'
+path = os.path.dirname(os.path.realpath(__file__))
+
+
+def panic(msg):
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
 
 if len(sys.argv) != 2:
     print("Usage: gen-matrix.py <type>")
     sys.exit(1)
 
-path = os.path.dirname(os.path.realpath(__file__))
-
-
-def rmprefix(s):
-    return s[len(PREFIX):] if s.startswith(PREFIX) else s
-
 
 if sys.argv[1] == 'applications':
-    filepath = os.path.join(path, '../applications/applications.yaml')
-    applications = yaml.safe_load(open(filepath))
-    applications = [rmprefix(a['fw_bundle'])
-                    for a in applications if 'fw_bundle' in a]
+    applications = []
+    for name in os.listdir(os.path.join(path, '../applications')):
+        if name.startswith('_') or '.' in name:
+            continue
+        project_yml_path = os.path.join(path, '../applications', name , 'project.yaml')
+        if os.path.exists(project_yml_path):
+            project = yaml.safe_load(open(project_yml_path, 'r'))
+            if not project.get('variants', None):
+                panic(f"Project {name} has no variants")
+            for variant in project['variants']:
+                fw_bundle = variant.get('fw_bundle', '')
+                if name not in fw_bundle:
+                    panic(f"Invalid fw_bundle name: name: {name} variant {variant['name']}")
+                if not fw_bundle.startswith(PREFIX):
+                    panic(f"Invalid fw_bundle prefix: name: {name} variant {variant['name']}")
+                applications.append(fw_bundle[len(PREFIX):])
+
     print(json.dumps(applications, sort_keys=True))
 
 elif sys.argv[1] == 'samples':
